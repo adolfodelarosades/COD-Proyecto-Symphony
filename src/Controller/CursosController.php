f57bd91e2baa651fed78cdcdb7fca79c4494c83d
Adolfo de la Rosa Peña <adolfodelarosa2012@gmail.com>
2021-09-21 10:52:29 +0200

    Carga Dinamicamente los Cursos

--- src/Controller/CursosController.php
+++ src/Controller/CursosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CursoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class CursosController extends AbstractController
 {
     #[Route('/cursos', name: 'cursos')]
-    public function index(): Response
+    public function index(CursoRepository $cursoRepository): Response
     {
+        $cursos = $cursoRepository->findAll();
+
         return $this->render('cursos/index.html.twig', [
-            'controller_name' => 'CursosController',
+            'cursos' => $cursos,
         ]);
     }
 }
